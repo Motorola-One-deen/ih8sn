691d10d158a10b2ebc7c4a5e856a7df4c25f63ea
Vhmit <viktor.9630@hotmail.com>
2023-10-18 16:01:04 -0300

    ih8sn: Add patches to safetynet
    Change-Id: Id5ff3cf5b6a1b77cf7bf1f6cb511ceadf387e0a4

--- patch.sh
+++ patch.sh
@@ -39,3 +39,5 @@ apply_patch() {
 }
 
 apply_patch system/security 0001-keystore-Block-key-attestation-for-Google-Play-Servi.patch
+apply_patch frameworks/base 0001-spoof-build-fingerprint-for-google-play-services.patch
+apply_patch frameworks/base 0002-use-fingerprint-walleye-pixel-2.patch
